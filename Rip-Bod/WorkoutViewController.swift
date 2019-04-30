3e51b88e91f6ab117225f98b4590790cdb9d5ec3
Murali, Abhejay <207931@s020rmc148-14.local>
2019-04-30 11:30:03 -0500

    added navigation controller and changed the status bar to light color

--- Rip-Bod/WorkoutViewController.swift
+++ Rip-Bod/WorkoutViewController.swift
@@ -11,10 +11,23 @@ import UIKit
 
 class WorkoutViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        UITabBar.appearance().barTintColor = UIColor.black // your color
+        tabBarController?.tabBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
     
 }
